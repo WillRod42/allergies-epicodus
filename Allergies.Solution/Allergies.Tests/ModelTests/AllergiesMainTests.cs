eac7f66385697739a1f8a6c5da24be49a158fa07
William Rodriguez <william@rodriguez1.com>
2022-05-03 11:37:45 -0700

    Get allergies from score
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- Allergies.Solution/Allergies.Tests/ModelTests/AllergiesMainTests.cs
+++ Allergies.Solution/Allergies.Tests/ModelTests/AllergiesMainTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Allergies;
 
 namespace Allergies.Tests
@@ -6,6 +7,32 @@ namespace Allergies.Tests
   [TestClass]
   public class AllergiesTests
   {
-    
+    [TestMethod]
+    public void GetAllergies_PrintsAllAllergiesBasedOnScore_None()
+    {
+      AllergiesMain allergies = new AllergiesMain();
+      CollectionAssert.AreEqual(new List<string>() { "none" }, allergies.GetAllergies(0));
+    }
+
+    [TestMethod]
+    public void GetAllergies_PrintsAllAllergiesBasedOnScore_TomatoAllergy()
+    {
+      AllergiesMain allergies = new AllergiesMain();
+      CollectionAssert.AreEqual(new List<string>() { "Tomatoes" }, allergies.GetAllergies(16));
+    }
+
+    [TestMethod]
+    public void GetAllergies_PrintsAllAllergiesBasedOnScore_TomatoAndEggAllergy()
+    {
+      AllergiesMain allergies = new AllergiesMain();
+      CollectionAssert.AreEqual(new List<string>() { "Tomatoes", "Eggs" }, allergies.GetAllergies(17));
+    }
+
+    [TestMethod]
+    public void GetAllergies_PrintsAllAllergiesBasedOnScore_AllAllergies()
+    {
+      AllergiesMain allergies = new AllergiesMain();
+      CollectionAssert.AreEqual(new List<string>() { "Cats", "Pollen", "Chocolate", "Tomatoes", "Strawberries", "Shellfish", "Peanuts", "Eggs" }, allergies.GetAllergies(255));
+    }
   }
 }
--- Allergies.Solution/Allergies/Models/AllergiesMain.cs
+++ Allergies.Solution/Allergies/Models/AllergiesMain.cs
@@ -1,12 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 namespace Allergies
 {
   public class AllergiesMain
   {
-    public static void Main()
+    public List<string> GetAllergies(int allergyScore)
     {
-
+      if (allergyScore == 0)
+      {
+        return new List<string>() { "none" };
+      }
+      else
+      {
+        List<string> allergies = new List<string>();
+        
+        if (allergyScore >= 128)
+        {
+          allergies.Add("Cats");
+          allergyScore -= 128;
+        }
+        if (allergyScore >= 64)
+        {
+          allergies.Add("Pollen");
+          allergyScore -= 64;
+        }
+        if (allergyScore >= 32)
+        {
+          allergies.Add("Chocolate");
+          allergyScore -= 32;
+        }
+        if (allergyScore >= 16)
+        {
+          allergies.Add("Tomatoes");
+          allergyScore -= 16;
+        }
+        if (allergyScore >= 8)
+        {
+          allergies.Add("Strawberries");
+          allergyScore -= 8;
+        }
+        if (allergyScore >= 4)
+        {
+          allergies.Add("Shellfish");
+          allergyScore -= 4;
+        }
+        if (allergyScore >= 2)
+        {
+          allergies.Add("Peanuts");
+          allergyScore -= 2;
+        }
+        if (allergyScore >= 1)
+        {
+          allergies.Add("Eggs");
+          allergyScore -= 1;
+        }
+        
+        return allergies;
+      }
     }
   }
 }
